06ced833694c4e1f286c0314e65019d44c6f8a4f
Shishkin Andrey <shish27@yandex.ru>
2024-05-18 14:05:04 +0500

    change if to assert.equal

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,14 +1,11 @@
 // нажмите кнопку run чтобы запустить тесты
 // попробуйте изменять код функции / тестов, запуская проверки заново
+import { strict as assert } from 'node:assert';
 
 import { capitalize } from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize('hello'), 'Hello');
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize(''), '');
 
 console.log('Все тесты пройдены!');
